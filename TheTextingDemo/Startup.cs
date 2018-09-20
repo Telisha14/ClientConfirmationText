7860ed024a6cf831f8ad0a3f243f37b551bfbe5f
dhanpalan.naidu <dhanpalan.naidu@TERENCENAIDU>
2018-09-20 18:22:13 +0200

    commit

---
+++ TheTextingDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheTextingDemo.Startup))]
+namespace TheTextingDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
---
+++ TheTextingDemo/fonts/Models/ConfigurationSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+namespace TheTextingDemo.Models
+{
+    public class ConfigurationSettings
+    {
+
+
+        [Key]
+        [ScaffoldColumn(false)]
+        public int RecordID { get; set; }
+       
+        [Required(ErrorMessage = "API_KEY is required")]
+        [Display(Name = "API_KEY")]
+        public string API_KEY { get; set; }
+        [Required(ErrorMessage = "API_SECRET is required")]
+        public string API_SECRET { get; set; }
+         [Required(ErrorMessage = "To_Number  is required")]
+        [Display(Name = "To_Number")]
+        public double To_Number { get; set; }
+        //[DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Message  is required")]
+        public string TextMessage { get; set; }
+       
+
+
+
+
+
+    }
+}
